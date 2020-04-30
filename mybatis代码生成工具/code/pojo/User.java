ad94f7fa03494e9d29ca8b14ee473b05360d0025
DuDuPang <1719900603@qq.com>
2020-04-30 16:48:49 +0800

    更新
    修正一些错误

---
+++ mybatis代码生成工具/code/pojo/User.java
@@ -0,0 +1,43 @@
+package code.pojo;
+
+public class User {
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    private String perm;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPerm() {
+        return perm;
+    }
+
+    public void setPerm(String perm) {
+        this.perm = perm == null ? null : perm.trim();
+    }
+}
